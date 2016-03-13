dc2c728cccdcf70292795dc62b0ce1dcf1ad9427
Ryan Coon <rybread354@aol.com>
2016-03-13 12:33:32 -0500

    Add SnapeAndLadder

---
+++ java_problems/beginner/SnapeAndLadder.java
@@ -0,0 +1,69 @@
+package java_problems.beginner;
+
+import java.util.Scanner;
+
+/*
+ * Professor Snape has lots of potions. Bottles containing all types of potions
+ * are stacked on shelves which cover the entire wall from floor to ceiling.
+ * Professor Snape has broken his bones several times while climbing the top
+ * shelf for retrieving a potion. He decided to get a ladder for him. But he has
+ * no time to visit Diagon Alley. So he instructed Ron Weasley to make a ladder
+ * for him. Professor Snape specifically wants a step ladder which looks like an
+ * inverted 'V' from side view.
+ * 
+ * Professor just mentioned two things before vanishing-
+ * B - separation between left side (LS) and right side (RS) on the ground
+ * LS - the length of left side
+ * 
+ * What should be the length of RS? At one extreme LS can be vertical and at
+ * other RS can be vertical. Ron is angry and confused. Since Harry is busy
+ * battling Voldemort, its your duty to help him find the minimum and maximum
+ * length of RS.
+ * 
+ * Input: 
+ * First line contains single integer T, the number of test cases. Then T lines
+ * follow each containing 2 integers - B and LS.
+ * 
+ * Output: 
+ * Output T lines, each containing minimum value of RS and maximum value of RS,
+ * separated by space. The answer (RS) will be considered correct if it has
+ * relative and absolute error less than 10-2.
+ * 
+ * Constraints: 
+ * 1 <= T <= 1000
+ * 1 <= B < LS <= 1000
+ * 
+ * Example 
+ * Input:
+ * 3
+ * 4 5
+ * 10 12
+ * 10 20
+ * 
+ * Output:
+ * 3.0 6.40312
+ * 6.63325 15.6205
+ * 17.3205 22.3607
+ */
+public class SnapeAndLadder {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int numCases = Integer.parseInt(sc.nextLine());
+		
+		for (int i = 0; i < numCases; i++) {
+			int B = sc.nextInt();
+			int LS = sc.nextInt();
+			outputMinMaxRS(B, LS);
+			System.out.println();
+		}
+		sc.close();
+	}
+	
+	private static void outputMinMaxRS(int B, int LS) {
+		double min = Math.sqrt((LS * LS) - (B * B));
+		double max = Math.sqrt((B * B) + (LS * LS));
+		System.out.printf("%.5f %.5f", min, max);
+	}
+}
