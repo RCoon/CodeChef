1f4a179fbf5236d04b14122070b020f41ba1fa1e
Ryan Coon <rybread354@aol.com>
2016-04-18 17:59:32 -0500

    Add NumberMirror

---
+++ java_problems/beginner/NumberMirror.java
@@ -0,0 +1,14 @@
+package java_problems.beginner;
+
+import java.util.Scanner;
+
+public class NumberMirror {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		System.out.println(num);
+		sc.close();
+	}
+}
